7b1e5925a38f6e856dddb3d442ec3acc2ef0ba17
Mohd-Arish <HealthHub>
2024-07-06 13:07:24 +0530

    first commit

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 export const dbConnection= ()=>{
   mongoose
     .connect(process.env.MONGO_URI, {
-        dbName: "HEALTHHUB_HOSPITAL_MANAGEMENT_SYSTEM_DEPLOYED"
+        dbName: "HEALTHHUB_HMS_HOSTED"
     })
     .then(()=>{
         console.log("Connected to database")
